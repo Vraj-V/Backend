45f254c8556b19a36af450c5045258439648e651
Vraj-V <vrajv2108@gmail.com>
2025-09-11 19:26:33 +0530

    new changes

--- Express js/index.js
+++ Express js/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -16,6 +16,10 @@ app.get("/character", function(req, res) {
     res.render("character");  // <-- pass variable
 });
 
+
+app.get("/shop", function(req, res) {
+    res.render("shop");  // <-- pass variable
+});
 app.listen(5000,function(){
     console.log("Server is running!");
 
