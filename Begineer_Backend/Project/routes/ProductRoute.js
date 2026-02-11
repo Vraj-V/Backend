2836fbf09a0806243a6a6902a0d27daf9c5accb2
Vraj Valand <123581157+Vraj-V@users.noreply.github.com>
2026-02-11 16:25:33 +0000

    Backend Adjusted

--- Begineer_Backend/Project/routes/ProductRoute.js
+++ Begineer_Backend/Project/routes/ProductRoute.js
@@ -1,12 +1,11 @@
 import express from "express";
 const Router  = express.Router();
+const app = express()
 
-let products = [
+app.set('view enginer','ejs');
+let products = [ 
     { id: 1, name: "Phone", price: 20000 },
-    { id: 2, name: "Laptop", price: 50000 },
-    { id: 3, name: "Tablet", price: 40000 },
-    { id: 4, name: "IPad", price: 60000 },
-    { id: 5, name: "IWatch", price: 23000 },
+    { id: 2, name: "Laptop", price: 50000 }
 ];
 
 Router.get('/',(req,res)=>{
@@ -16,7 +15,7 @@ Router.get('/',(req,res)=>{
 Router.get('/:id', (req,res)=>{
     const id  = parseInt(req.params.id);
 
-    const product = products.find(p => p.id = id);
+    const product = products.find(p => p.id === id);
 
     if(!product){
         return res.status(404).json({Error: 'No product found!'});
@@ -24,5 +23,13 @@ Router.get('/:id', (req,res)=>{
 
     res.json(product)
 })
+
+
+
+Router.post('/',(req,res)=>{
+    products.push(req.body);
+    res.status(201).json(req.body)
+});
+
 
 export default Router;
