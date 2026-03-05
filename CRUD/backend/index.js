f82c3686a7166b77ad3f64e867709b86546bdd28
Vraj-V <vrajv2108@gmail.com>
2026-03-05 22:15:07 +0530

    backend

--- CRUD/backend/index.js
+++ CRUD/backend/index.js
@@ -9,14 +9,29 @@ app.use(express.json());
 
 mongoose.connect("mongodb+srv://vraj230183107024_db_user:wk1Aw23uTfpGAAja@cluster0.ivi3brc.mongodb.net/Mern");
 
-app.get("/getUsers",(req,res)=>{
-    userModel.find({})
-    .then((result)=>{
-        res.status(200).json(result);
-    })
-    .catch((err)=> res.json(err));
+app.get("/getUsers",async (req,res)=>{
+    try{
+        const user = await userModel.find({})
+        res.status(200).json(user)
+
+    }
+    catch(err){
+        res.status(400).json(err)
+    }
 })
 
+
+app.post("/createUser",async (req,res)=>{
+    try{
+    const user = req.body;
+    const newUser = new userModel(user);
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+    }
+    catch(err){
+        res.status(400).json(err)
+    }
+})
 app.listen(3000,()=>{
     console.log("server is running 3000!")
 })
