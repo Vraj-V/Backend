226fcf9bff0fb0e0adf6fa1acaebae673b8c078f
Vraj-V <vrajv2108@gmail.com>
2026-03-06 21:40:34 +0530

    server

---
+++ CRUD/second_project/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userModel = require('./models/Operation');
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect("mongodb://localhost:27017/crud")
+
+
+app.get("/", async (req,res)=>{
+    try{
+        const result = await userModel.find({})
+    res.status(200).json(result)
+    }
+    catch(err){
+        res.status(400).json(err);
+        console.log(err)
+    }
+})
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
